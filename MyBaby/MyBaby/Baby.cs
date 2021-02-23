d5189753f716ea1dbf8600c970ba80a8908f6182
BluEiS <thequy@hotmail.com>
2021-02-23 10:06:11 +0700

    End of Coding

---
+++ MyBaby/MyBaby/Baby.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBaby
+{
+    public class Baby
+    {
+        public string NickName = "";
+        public string Gender = "";
+        public string DOB = "";
+        public double Weight = 0.0;
+        public void Say(string Text)
+        {
+            
+        }
+    }
+}
---
+++ MyBaby/MyBaby/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBaby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby myLittleGirl = new Baby();
+            myLittleGirl.NickName = "Thỏ";
+            myLittleGirl.Gender = "GIRL";
+            myLittleGirl.Weight = 3.4;          //kg
+            myLittleGirl.DOB = "12/01/2017";    //15h45 ngày 15/12 năm Bính Thân
+
+            myLittleGirl.Say("HELLO WORLD !");
+        }
+    }
+}
